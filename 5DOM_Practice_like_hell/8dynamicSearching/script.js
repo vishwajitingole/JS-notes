8d2eea66a6aa8a36cf0ac86b7593600f606bd208
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-12-27 10:24:25 +0530

    dynamicSearching

--- 5DOM_Practice_like_hell/8dynamicSearching/script.js
+++ 5DOM_Practice_like_hell/8dynamicSearching/script.js
@@ -16,6 +16,26 @@ data = [
     name: "stella",
     src: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df",
   },
+  {
+    name: "sabrina",
+    src: "https://images.unsplash.com/photo-1638431919260-af6cbc16dcfc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHNleHklMjBnaXJscyUyMHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    name: "becca",
+    src: "https://images.unsplash.com/photo-1645342682688-247f1172166a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHNleHklMjBnaXJscyUyMHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    name: "joanna",
+    src: "https://images.unsplash.com/photo-1549971362-9ce69d0fb764?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fHNleHklMjBnaXJscyUyMHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    name: "jessie",
+    src: "https://images.unsplash.com/photo-1697921814050-c48df12df6b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjB8fHNleHklMjBnaXJscyUyMHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    name: "barbie",
+    src: "https://plus.unsplash.com/premium_photo-1661548271175-6163149fc57c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHNleHklMjBnaXJscyUyMHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
 ];
 var person = ""; //so our person doesn't have an undefined value
 data.forEach(function (elem) {
